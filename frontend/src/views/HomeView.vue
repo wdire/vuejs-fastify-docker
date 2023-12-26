e8021daa0d6ae1777186d9beb8c8d6f9bda753d1
Ramazan <wdire@t-x.local>
2023-12-26 23:41:14 +0300

    added frontend

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue'
+import axios from 'axios'
+
+const categories = ref(null)
+
+watchEffect(async () => {
+  categories.value = (await axios.get('http://localhost:5500/api/category/get-all')).data
+})
+</script>
+
+<template>
+  <main>
+    <pre>{{ categories && JSON.stringify(categories, null, 1) }}</pre>
+  </main>
+</template>
