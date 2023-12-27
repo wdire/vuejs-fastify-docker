af24a87d0d07f397d1fff5677f01bcfa04509e51
Ramazan <wdire@t-x.local>
2023-12-27 14:46:59 +0300

    setup docker for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      port: env.PORT ? Number(env.PORT) : undefined,
+      host: true
+    },
+    preview: {
+      port: env.PORT ? Number(env.PORT) : undefined,
+      host: true
     }
   }
 })
